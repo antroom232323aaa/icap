c84cdac336aa5a6610d1899d4194c694eabe31df
antroom232323aaa <antroom232323aaa@gmail.com>
2026-08-18 10:56:17 +0800

    0818 部署測試

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,7 +11,7 @@
 
     {{-- Bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
 
     @stack('styles')
 </head>
